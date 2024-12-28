c312802fb801ae46beba1756327bebc82db9d90f
Maverick Madison <134321883+Gawdlie@users.noreply.github.com>
2024-12-28 15:16:23 -0700

    Incorporate 5 score limit
    Implemented logic for when either the user or computer reaches a score of 5.

--- Rock Paper Scissors UI/script.js
+++ Rock Paper Scissors UI/script.js
@@ -7,34 +7,89 @@ const paperButton = document.createElement("button");
 const scissorsButton = document.createElement("button");
 const roundResults = document.createElement("div");
 const runningScore = document.createElement("div");
+const container = document.createElement("div");
+const buttonContainer = document.createElement("div");
+const resultsContainer = document.createElement("div");
 
 document.body.appendChild(rockButton);
 document.body.appendChild(paperButton);
 document.body.appendChild(scissorsButton);
 document.body.appendChild(roundResults);
 document.body.appendChild(runningScore);
+document.body.appendChild(container);
+document.body.appendChild(buttonContainer);
+document.body.appendChild(resultsContainer);
 
 rockButton.textContent = "Rock";
 paperButton.textContent = "Paper";
 scissorsButton.textContent = "Scissors";
-roundResults.style.border = "solid";
 roundResults.textContent = "this is where the results will be";
-runningScore.style.border = "solid";
+roundResults.style.cssText = "font-size: 30px; color: white; font-family: Arial, Helvetica, sans-serif;"
 runningScore.textContent = "this is where the running score will be";
+runningScore.style.cssText = "font-size: 30px; color: white; font-family: Arial, Helvetica, sans-serif;"
+
+document.body.style.cssText = "display: flex; justify-content: center; align-items: center; background-color: maroon; margin: 0px; padding: 0px;"
+
+container.appendChild(rockButton);
+container.appendChild(paperButton);
+container.appendChild(scissorsButton);
+container.appendChild(roundResults);
+container.appendChild(runningScore);
+container.appendChild(buttonContainer);
+container.appendChild(resultsContainer);
+container.style.cssText = "margin-top: 240px;";
+
+buttonContainer.appendChild(rockButton);
+buttonContainer.appendChild(paperButton);
+buttonContainer.appendChild(scissorsButton);
+buttonContainer.style.cssText = "display: flex; justify-content: center; align-items: center; gap: 20px; padding: 30px;";
+
+resultsContainer.appendChild(roundResults);
+resultsContainer.appendChild(runningScore);
+resultsContainer.style.cssText = "display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 20px; padding: 30px;";
+
+rockButton.style.cssText = "padding: 20px; border-radius: 20px; cursor: pointer;";
+paperButton.style.cssText = "padding: 20px; border-radius: 20px; cursor: pointer;";
+scissorsButton.style.cssText = "padding: 20px; border-radius: 20px; cursor: pointer;";
+
 
 rockButton.addEventListener("click", () => {
-  playRound("rock", getComputerChoice());
-  roundResults.textContent = "rock vs " + computerSelectionVar;
+  if(humanScore === 5) {
+    roundResults.textContent = "You win!"
+  }
+  else if(computerScore === 5) {
+    roundResults.textContent = "Computer wins!"
+  }
+  else {
+    playRound("rock", getComputerChoice());
+    roundResults.textContent = "rock vs " + computerSelectionVar;
+  }
 });
 
 paperButton.addEventListener("click", () => {
-  playRound("paper", getComputerChoice());
-  roundResults.textContent = "paper vs " + computerSelectionVar;
+  if(humanScore === 5) {
+    roundResults.textContent = "You win!"
+  }
+  else if(computerScore === 5) {
+    roundResults.textContent = "Computer wins!"
+  }
+  else {
+    playRound("paper", getComputerChoice());
+    roundResults.textContent = "paper vs " + computerSelectionVar;
+  }
 });
 
 scissorsButton.addEventListener("click", () => {
-  playRound("scissors", getComputerChoice());
-  roundResults.textContent = "scissors vs " + computerSelectionVar;
+  if(humanScore === 5) {
+    roundResults.textContent = "You win!"
+  }
+  else if(computerScore === 5) {
+    roundResults.textContent = "Computer wins!"
+  }
+  else {
+    playRound("scissors", getComputerChoice());
+    roundResults.textContent = "scissors vs " + computerSelectionVar;
+  }
 });
 
 function getComputerChoice() {
